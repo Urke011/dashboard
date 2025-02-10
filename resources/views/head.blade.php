d8ccb6cac4b53336b45fc1507e66493f1fb3f4cd
Uros Rajkovic <uros.rajkovic@mathema.de>
2025-02-10 16:54:33 +0100

    prepore vite config for local and production

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -8,5 +8,13 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    @vite(['resources/sass/app.scss','resources/js/app.js'])
+    @if (env('APP_ENV') === 'local')
+        <!-- Vite dev server for local -->
+        @vite(['resources/js/app.js', 'resources/sass/app.scss'])
+    @else
+        <!-- Production assets -->
+        <link href="{{ asset('build/assets/app-C5PQxqkl.css') }}" rel="stylesheet">
+        <link href="{{ asset('build/assets/app-tBZnU75t.css') }}" rel="stylesheet">
+        <script src="{{ asset('build/assets/app-XD3OQ2eG.js') }}" type="module"></script>
+    @endif
 </head>
