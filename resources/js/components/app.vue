d7ec3f507e5bc342556686e9ef70e40cdcccc8c1
Uros Rajkovic <uros.rajkovic@mathema.de>
2025-02-12 09:24:17 +0100

    remove all

--- resources/js/components/app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h2>
-        VueSchool Rocks! 🤘
-    </h2>
-</template>
--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\StockController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [StockController::class, 'getAllDashboardValue'])->name('welcome');
-
-Route::get('/todo', [StockController::class, 'createTodoTaskInputs'])->name('todo.create');
-Route::post('/todo', [StockController::class, 'storeTodoTaskInputs'])->name('todo.store');
