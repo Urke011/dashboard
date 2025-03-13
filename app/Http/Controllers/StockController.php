ca30b30eba5431dcf9ea0b2359183999820f86c4
urajkovic <uros.rajkovic@mathema.de>
2025-03-13 13:42:42 +0100

    add apple stock

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -57,7 +57,7 @@ class StockController extends Controller
     {
         //max 25 calls
         $apiKey = config('services.api_stock_service.key');
-        $symbols = ["MSFT", "MCD","KO"];
+        $symbols = ["MSFT", "MCD","KO",'AAPL'];
         $interval = "5min"; // Use a supported interval like 1min, 5min, 15min, etc.
 
         try {
