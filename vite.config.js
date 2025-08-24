7f5c7d08d2124ac8120a4dd0e71060cc54d85a93
urosdeveloper <uros.rajkovic@mathema.de>
2025-08-24 11:03:13 +0200

    add new vite file

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,31 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import dotenv from 'dotenv';
 
-// Load environment variables from Laravel's .env
-dotenv.config();
+export default defineConfig(({ mode }) => {
+    const isProduction = mode === 'production';
 
-const isProduction = process.env.APP_ENV === 'production';
-
-export default defineConfig({
-    plugins: [
-        vue(),
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/sass/app.scss'],
-            refresh: !isProduction, // Enable live reload only in non-production
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+    return {
+        plugins: [
+            vue(),
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                    'resources/sass/app.scss',
+                ],
+                refresh: !isProduction,
+            }),
+        ],
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.esm-bundler.js',
+            },
         },
-    },
-    base: isProduction ? '/build/' : '/', // Use correct base URL for assets
+        build: {
+            outDir: 'public/build',
+            manifest: true,
+            emptyOutDir: true,
+        },
+    };
 });
