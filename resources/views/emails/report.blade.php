096aa215b660b1bfa3511f9c5258743626ce724f
Uros Rajkovic <uros.rajkovic@mathema.de>
2025-09-08 08:20:13 +0200

    remove tags from td

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -15,7 +15,7 @@
                 {{ $data['category']->label }}
             </td>
             <td style="border: 1px solid #ccc; padding: 8px; text-align: right;">
-                <span>{{ number_format($data['totalAmount'], 0, ',', '.') }}</span><span style="white-space:nowrap;">rsd</span>
+                {{ number_format($data['totalAmount'], 0, ',', '.') }}
             </td>
             <td style="border: 1px solid #ccc; padding: 8px; text-align: right;">
                 {{ $data['percent'] }}%
